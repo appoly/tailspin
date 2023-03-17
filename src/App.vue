ad7cf768b097ff7de3bdc53ca4e2a960099e1f91
Nathan James <nathanjms@outlook.com>
2023-03-17 12:32:41 +0000

    Updates

--- src/App.vue
+++ src/App.vue
@@ -12,26 +12,104 @@
   </div>
 
   <div>
-    <pre>{{ log }}</pre>
+    <pre>{{ logEntries.slice(0, 5) ?? [] }}</pre>
   </div>
 </template>
 
 <script setup lang="ts">
 import { readFile } from "fs/promises";
 import { ref, onMounted } from "vue";
+import { LogStatuses } from "./constants/LogStatuses"
+
+interface LogEntry {
+  timestamp: string;
+  severity: string;
+  level: string;
+  text: string;
+}
+
+onMounted(async () => {
+  const file = await content("storage/laravel.log");
+  log.value = file;
+  logEntries.value = parseLogEntries(file);
+});
 
 const log = ref("");
+const logEntries = ref<LogEntry[]>([]);
 
 async function content(path: string): Promise<string> {
   return await readFile(path, "utf8");
 }
 
-onMounted(async () => {
-  console.log(__dirname);
-  const file = await content("storage/laravel.log");
-  log.value = file;
-  console.log(log.value.split("\n")[0]);
-});
+const dateTimestampRegex = new RegExp(/^\[\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2}\]/);
+
+/**
+ * This pattern, used for processing Laravel logs, returns these results:
+ * $matches[0] - the full log line being tested.
+ * $matches[1] - full timestamp between the square brackets (includes microseconds and timezone offset)
+ * $matches[2] - timestamp microseconds, if available
+ * $matches[3] - timestamp timezone offset, if available
+ * $matches[4] - contents between timestamp and the severity level
+ * $matches[5] - environment (local, production, etc)
+ * $matches[6] - log severity (info, debug, error, etc)
+ * $matches[7] - the log text, the rest of the text.
+ */
+
+console.log(LogStatuses.join('|'));
+
+const logParsingRegex = new RegExp(`^\\[(\\d{4}-\\d{2}-\\d{2}[T ]\\d{2}:\\d{2}:\\d{2}\\.?(\\d{6}([\\+-]\\d\\d:\\d\\d)?)?)\\](.*?(\\w+)\\.|.*?)(` + LogStatuses.join('|') + `)?: (.*?)( in [\\/].*?:[0-9]+)?$`, 'i');
+
+/**
+ * Parse log entries from log file
+ * @param logData
+ */
+function parseLogEntries(logData: string) {
+  // First split by new lines. A lot of logs are on one line, but some are not. This is much more efficient than splitting by the date.
+  const logEntries = logData.split("\n").filter(line => line.trim() !== '');
+  const parsedEntries: LogEntry[] = [];
+  const entries: {
+    timestamp: string;
+    level: string;
+    severity: string;
+    text: string;
+  }[] = [];
+  let entryIndex = 0;
+
+  const currentMessage: string[][] = [];
+
+  for (let i = 0; i < logEntries.length; i++) {
+    const entry = logEntries[i];
+    // Skip rows that don't start with a date when it is the first entry
+
+    if (entryIndex === 0 && !entry.match(dateTimestampRegex)) {
+      continue;
+    }
+
+    if (entryIndex > 0) continue;
+    // If it matches the date regex, it is a new entry)
+    let timestamp = entry.match(dateTimestampRegex)?.[0] ?? null;
+
+    if (timestamp) {
+      // If we have found a timestamp match, lets call the complicated variable match
+      let matches = entry.match(logParsingRegex);
+
+      parsedEntries[entryIndex] = {
+        timestamp,
+        level: matches?.[5] ?? 'unknown',
+        severity: matches?.[6] ?? 'unknown',
+        text: matches?.[7] ?? '',
+      };
+      entryIndex++;
+    } else {
+      // Otherwise, it is part of the message, so add it to the message of the previous entry
+      parsedEntries[entryIndex].text += "\n" + entry;
+    }
+
+  }
+  return parsedEntries;
+}
+
+
 </script>
 
 <style lang="scss" scoped></style>
---
+++ src/constants/LogStatuses.ts
@@ -0,0 +1,13 @@
+export const LogStatuses = Object.freeze([
+  "debug",
+  "info",
+  "notice",
+  "warning",
+  "error",
+  "critical",
+  "alert",
+  "emergency",
+  "processing",
+  "processed",
+  "failed",
+]);
