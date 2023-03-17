7992e1b0bb46666d6b51c492945da966788a6ca2
Nathan James <nathanjms@outlook.com>
2023-03-17 11:20:43 +0000

    Read file

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,37 @@
 <template>
-	<nav class="navbar navbar-expand-lg bg-body-tertiary">
-		<div class="container-fluid">
-			<a class="navbar-brand" href="#">Log Viewer</a>
-		</div>
-	</nav>
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Log Viewer</a>
+    </div>
+  </nav>
 
-	<div class="container-fluid my-4">
-		<div class="input-group mb-3">
-			<input ref="logFile" type="file" class="form-control" />
-		</div>
-	</div>
+  <div class="container-fluid my-4">
+    <div class="input-group mb-3">
+      <input ref="logFile" type="file" class="form-control" />
+    </div>
+  </div>
+
+  <div>
+    <pre>{{ log }}</pre>
+  </div>
 </template>
 
-<script>
-export default {
-	data() {
-		return {
-			log: ''
-		}
-	},
-	mounted() {
-		this.$refs.logFile.addEventListener('change', this.handleFileUpload, false);
-	},
-	methods: {
-		handleFileUpload(e) {
-			console.log('reading file...');
-			const file = e.target.files[0];
-			const reader = new FileReader();
-			reader.onload = (e) => {
-				console.log(e.target.result);
-			};
-			this.log = reader.readAsText(file);
-		}
-	}
+<script setup>
+import { readFile } from "fs/promises";
+import { ref, onMounted } from "vue";
+
+const log = ref("");
+
+async function content(path) {
+  return await readFile(path, "utf8");
 }
+
+onMounted(async () => {
+  console.log(__dirname);
+  const file = await content("storage/laravel.log");
+  log.value = file;
+  console.log(log.value.split("\n")[0]);
+});
 </script>
 
 <style lang="scss" scoped></style>
