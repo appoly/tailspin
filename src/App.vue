6c32f04de95ca5f57c67f94466c746171785e40e
Calum Chamberlain <calum@appoly.co.uk>
2023-03-17 11:27:31 +0000

    read from file select

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,19 @@
 <template>
-  <nav class="navbar navbar-expand-lg bg-body-tertiary">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="#">Log Viewer</a>
-    </div>
-  </nav>
+	<nav class="navbar navbar-expand-lg bg-body-tertiary">
+		<div class="container-fluid">
+			<a class="navbar-brand" href="#">Log Viewer</a>
+		</div>
+	</nav>
 
-  <div class="container-fluid my-4">
-    <div class="input-group mb-3">
-      <input ref="logFile" type="file" class="form-control" />
-    </div>
-  </div>
+	<div class="container-fluid my-4">
+		<div class="input-group mb-3">
+			<input id="logFile" type="file" class="form-control" />
+		</div>
+	</div>
 
-  <div>
-    <pre>{{ log }}</pre>
-  </div>
+	<div>
+		<pre>{{ log }}</pre>
+	</div>
 </template>
 
 <script setup lang="ts">
@@ -23,14 +23,21 @@ import { ref, onMounted } from "vue";
 const log = ref("");
 
 async function content(path: string): Promise<string> {
-  return await readFile(path, "utf8");
+	return await readFile(path, "utf8");
+}
+
+async function handleFileSelect(evt: any) {
+	const files = evt.target.files; // FileList object
+	const file = files[0];
+	const filePath = file.path;
+
+	const fileContent = await content(filePath);
+	log.value = fileContent;
 }
 
 onMounted(async () => {
-  console.log(__dirname);
-  const file = await content("storage/laravel.log");
-  log.value = file;
-  console.log(log.value.split("\n")[0]);
+	const fileInput = document.getElementById("logFile");
+	fileInput.addEventListener("change", handleFileSelect, false);
 });
 </script>
 
