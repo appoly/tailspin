e96d3dde35e4538f4a4245b647961dd3a6d910d7
Calum Chamberlain <calum@appoly.co.uk>
2023-03-24 15:20:49 +0000

    icon picker

--- src/pages/SettingsPage.vue
+++ src/pages/SettingsPage.vue
@@ -1,10 +1,13 @@
 <template>
-    <div>
+    <div class="mb-5">
         <button @click="userStore.toggleTheme" class="btn btn-primary">Toggle Theme</button>
     </div>
+
+    <BootstrapIconPickerVue></BootstrapIconPickerVue>
 </template>
 
 <script setup lang="ts">
+import BootstrapIconPickerVue from '@/components/BootstrapIconPicker.vue';
 import { useUserStore } from '@/stores/useUserStore';
 
 const userStore = useUserStore();
