de0ec385f0a6ba016a2381dc770cb926356b241f
Nathan James <nathanjms@outlook.com>
2023-03-24 11:28:00 +0000

    Tooltips

--- src/components/TheSideBar.vue
+++ src/components/TheSideBar.vue
@@ -3,15 +3,15 @@
         <ul class="nav nav-pills nav-flush flex-column mb-auto text-center">
             <li class="nav-item">
                 <a href="#" class="nav-link active py-3 border-bottom rounded-0" aria-current="page"
-                    data-bs-toggle="tooltip" data-bs-placement="right" aria-label="Example"
-                    data-bs-original-title="Example">
-                    Example
+                    data-bs-toggle="tooltip" data-bs-placement="right" aria-label="Add new connection"
+                    data-bs-original-title="Add new connection">
+                    <i class="bi bi-plus-square h2"></i>
                 </a>
             </li>
         </ul>
         <div class="dropdown border-top">
-            <a href="#" class="nav-link active py-3 border-bottom rounded-0" aria-current="page" data-bs-toggle="tooltip"
-                data-bs-placement="right" aria-label="Settings" data-bs-original-title="Settings">
+            <a href="#" class="nav-link text-center active py-3 border-bottom rounded-0" aria-current="page"
+                data-bs-toggle="tooltip" data-bs-placement="right" aria-label="Settings" data-bs-original-title="Settings">
                 <i class="bi bi-gear-wide-connected h2"></i>
             </a>
         </div>
@@ -19,7 +19,14 @@
 </template>
 
 <script setup lang="ts">
+import { Tooltip } from 'bootstrap'
+import { onMounted } from 'vue';
 
+onMounted(() => {
+    new Tooltip(document.body, {
+        selector: "[data-bs-toggle='tooltip']",
+    });
+})
 </script>
 
 <style scoped></style>
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { createApp } from "vue";
 import "./app.scss";
-import "bootstrap";
 import "bootstrap-icons/font/bootstrap-icons.css";
 
 import App from "./App.vue";
