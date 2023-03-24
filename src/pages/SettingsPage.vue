956e9726e956502e1c7b46c54c0eab0bbf6119b9
Calum Chamberlain <calum@appoly.co.uk>
2023-03-24 15:21:11 +0000

    Update SettingsPage.vue

--- src/pages/SettingsPage.vue
+++ src/pages/SettingsPage.vue
@@ -1,13 +1,10 @@
 <template>
-    <div class="mb-5">
+    <div>
         <button @click="userStore.toggleTheme" class="btn btn-primary">Toggle Theme</button>
     </div>
-
-    <BootstrapIconPickerVue></BootstrapIconPickerVue>
 </template>
 
 <script setup lang="ts">
-import BootstrapIconPickerVue from '@/components/BootstrapIconPicker.vue';
 import { useUserStore } from '@/stores/useUserStore';
 
 const userStore = useUserStore();
