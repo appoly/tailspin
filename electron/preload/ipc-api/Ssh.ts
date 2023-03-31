684f8ea40663cb87fa0d5cca8814b67f998c1c57
Nathan James <nathanjms@outlook.com>
2023-03-31 11:48:31 +0100

    Fixes

--- electron/preload/ipc-api/Ssh.ts
+++ electron/preload/ipc-api/Ssh.ts
@@ -1,5 +1,15 @@
 import { ipcRenderer } from "electron";
-import { SshDetails } from "../../interfaces";
+
+/**
+ * I'm not too happy with this interface and the one inside the interfaces file in src being so similar, but it works for now.
+ */
+interface SshDetails {
+  host: string;
+  port: number;
+  username: string;
+  passwordType: "password" | "key";
+  password: string;
+}
 
 export function testSshCredentials(options: SshDetails): Promise<boolean> {
   return ipcRenderer.invoke("test-ssh-credentials", options);
