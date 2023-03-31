75d4503a91770949c0884cc6169b04bf9de74271
Nathan James <nathanjms@outlook.com>
2023-03-31 15:03:24 +0100

    Handle folder and file

--- electron/preload/ipc-api/Ssh.ts
+++ electron/preload/ipc-api/Ssh.ts
@@ -11,6 +11,23 @@ interface SshDetails {
   password: string;
 }
 
+interface SshResponse {
+  success: boolean;
+  message?: string;
+}
+
 export function testSshCredentials(options: SshDetails): Promise<{ success: boolean; error?: any }> {
   return ipcRenderer.invoke("test-ssh-credentials", options);
+}
+
+export function isFileOrDirectory(options: SshDetails, path: string): Promise<SshResponse> {
+  return ipcRenderer.invoke("ssh-is-file-or-directory", options, path);
+}
+
+export function getFilesInDirectory(options: SshDetails, path: string): Promise<SshResponse> {
+  return ipcRenderer.invoke("ssh-get-files-in-directory", options, path);
+}
+
+export function readFromPath(options: SshDetails, path: string): Promise<SshResponse> {
+  return ipcRenderer.invoke("ssh-read-from-path", options, path);
 }
