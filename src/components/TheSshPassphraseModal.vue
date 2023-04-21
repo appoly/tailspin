766213f864ad1c16e69c79875f36cc86b358cb03
Nathan James <nathanjms@outlook.com>
2023-04-21 14:03:06 +0100

    Passphrase modal info

--- src/components/TheSshPassphraseModal.vue
+++ src/components/TheSshPassphraseModal.vue
@@ -3,6 +3,11 @@
         <div class="modal-dialog modal-dialog-centered">
             <div class="modal-content">
                 <div class="modal-body">
+                    <div class="alert alert-info" role="alert" v-if="showInfoMessage">
+                        <span>
+                            This connection requires an SSH passphrase. Please enter it below.
+                        </span>
+                    </div>
                     <form @submit.prevent="handleSubmit">
                         <label class="mb-2">Enter SSH Passphrase</label>
                         <div class="input-group">
@@ -10,6 +15,7 @@
                                 @input="handleInputChange" required aria-label="Passphrase" id="passphraseInput" />
                             <button class="btn btn-secondary">Go</button>
                         </div>
+                        <small><i>This will not be saved.</i></small>
                     </form>
                 </div>
             </div>
@@ -26,6 +32,7 @@ const emit = defineEmits(['update:modelValue', 'submit']);
 defineExpose({ open, close });
 
 const sshPassphraseModal = ref<Modal>();
+const showInfoMessage = ref(true);
 onMounted(() => {
     sshPassphraseModal.value = new Modal('#sshPassphraseModal');
     document.querySelector('#sshPassphraseModal')!.addEventListener('shown.bs.modal', focusOnInput)
@@ -42,7 +49,8 @@ function handleInputChange(event: Event) {
 function handleSubmit() {
     // Give time for the modelValue to change, then emit submit
     console.log('close');
-    
+    showInfoMessage.value = false;
+
     nextTick(() => {
         close();
         emit('submit');
@@ -51,11 +59,12 @@ function handleSubmit() {
 
 function open() {
     sshPassphraseModal.value?.show();
+    showInfoMessage.value = true;
 }
 
 function close() {
     console.log(sshPassphraseModal.value);
-    
+
     sshPassphraseModal.value?.hide();
 }
 
