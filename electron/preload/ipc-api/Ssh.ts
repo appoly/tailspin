69436727fabec7a41fe49f7bfcace810d5aa60c3
Nathan James <nathanjms@outlook.com>
2023-05-26 16:52:33 +0100

    Download Btn WIP

--- electron/preload/ipc-api/Ssh.ts
+++ electron/preload/ipc-api/Ssh.ts
@@ -36,3 +36,7 @@ export async function readFromPath(options: SshDetails, path: string): Promise<S
   let lines = await ipcRenderer.invoke("config-get", "ssh.numberOfLines", 1000);
   return ipcRenderer.invoke("ssh-read-from-path", options, path, lines);
 }
+
+export async function downloadFromPath(options: SshDetails, path: string, fileName: string): Promise<SshResponse> {
+  return ipcRenderer.invoke("ssh-download-from-path", options, path, fileName);
+}
