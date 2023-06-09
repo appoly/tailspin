74af3ca9485d218d714c03a3701a70330e4745f5
Nathan James <nathanjms@outlook.com>
2023-06-09 09:37:56 +0100

    Make forge section optional via a setting

--- src/components/settings/TheForgeApiKeyForm.vue
+++ src/components/settings/TheForgeApiKeyForm.vue
@@ -1,29 +1,46 @@
 <template>
-    <div class="form-group my-4">
-        <label for="forge-api-key">
-            Forge API Key
-        </label>
-        <div v-if="errorMsg" class="alert alert-danger" role="alert">
-            {{ errorMsg }}
-        </div>
-        <template v-if="currentKeyExists">
-            <button class="btn btn-outline-danger w-100" @click="clearKey">Clear current API Key</button>
-            <small class="text-muted">Clicking this will clear the current API key. You can then enter a new one.</small>
-        </template>
-        <template v-else>
-            <textarea v-model="forgeApiKey" class="form-control" id="forge-api-key" placeholder="Forge API Key" />
-            <small class="text-muted">
-                We can pull your sites from Forge using an API key. You can create one <a
-                    href="https://forge.laravel.com/user-profile/api" target="_blank">here</a>.
-            </small>
-            <button class="btn btn-primary w-100 mt-2" @click="submitKey">Save</button>
+    <div class="my-4">
+        <div class="form-group d-flex align-items-center gap-3 mb-2">
+            <span>Enable Forge Section</span>
+            <div class="btn-group flex-grow-1" role="group" aria-label="Basic radio toggle button group">
+                <input type="radio" class="btn-check" name="btnradio" id="btnradio1" autocomplete="off" :value="false"
+                    v-model="applicationStore.forgeSectionEnabled" @click="applicationStore.toggleForgeSectionEnabled">
+                <label class="btn btn-outline-primary" for="btnradio1">No</label>
 
-        </template>
+                <input type="radio" class="btn-check" name="btnradio" id="btnradio2" autocomplete="off" :value="true"
+                    v-model="applicationStore.forgeSectionEnabled" @click="applicationStore.toggleForgeSectionEnabled">
+                <label class="btn btn-outline-primary" for="btnradio2">Yes</label>
+            </div>
+        </div>
+        <div class="form-group">
+            <label for="forge-api-key">
+                Forge API Key
+            </label>
+            <div v-if="errorMsg" class="alert alert-danger" role="alert">
+                {{ errorMsg }}
+            </div>
+            <template v-if="currentKeyExists">
+                <button class="btn btn-outline-danger w-100" @click="clearKey">Clear current API Key</button>
+                <small class="text-muted">Clicking this will clear the current API key. You can then enter a new
+                    one.</small>
+            </template>
+            <template v-else>
+                <textarea v-model="forgeApiKey" class="form-control" id="forge-api-key" placeholder="Forge API Key" />
+                <small class="text-muted">
+                    We can pull your sites from Forge using an API key. You can create one <a
+                        href="https://forge.laravel.com/user-profile/api" target="_blank">here</a>.
+                </small>
+                <button class="btn btn-primary w-100 mt-2" @click="submitKey">Save</button>
+            </template>
+        </div>
     </div>
 </template>
 
 <script setup lang="ts">
-import { computed, onMounted, ref } from 'vue';
+import { useApplicationStore } from '@/stores/useApplicationStore';
+import { onMounted, ref } from 'vue';
+
+const applicationStore = useApplicationStore();
 
 /**
  * We do not want to show the actual API key if it is submitted, because it will be encrypted. Instead, allow the user to 'clear' the field.
@@ -60,4 +77,7 @@ async function submitKey() {
     }
 }
 
+function changeForgeEnabled() {
+    applicationStore.toggleForgeSectionEnabled();
+}
 </script>
